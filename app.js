61f15c293575979931396a7377acd5ab79e79e73
Andres Alcocer <andresalcocer7@yahoo.com>
2017-09-27 23:01:39 -0400

    add mongoLab

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes = require('./routes/comments.js'),
 
  //connects to database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/yelp_camp', { useMongoClient: true });
+//mongoose.connect('mongodb://localhost/yelp_camp', { useMongoClient: true });
+mongoose.connect('mongodb://andres:Barcelona10@ds155414.mlab.com:55414/yelp_camp', { useMongoClient: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
