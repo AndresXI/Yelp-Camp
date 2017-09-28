6007b9794b8b506f1f8c894dbd6bd3d0e2e5c421
Andres Alcocer <andresalcocer7@yahoo.com>
2017-09-28 12:38:48 -0400

    db user

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var commentRoutes = require('./routes/comments.js'),
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost/yelp_camp', { useMongoClient: true });
 //mongo lab database
-mongoose.connect('mongodb://andres:Barcelona10@ds155414.mlab.com:55414/yelp_camp', { useMongoClient: true });
+mongoose.connect('mongodb://andres:Barcelona10@ds061288.mlab.com:61288/yelp_camp', { useMongoClient: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
